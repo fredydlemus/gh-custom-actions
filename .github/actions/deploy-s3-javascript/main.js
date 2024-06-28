9534d3431b9889efc54a6c914438a9680b1b98cc
fredydlemus <fflores@eaglehubs.com>
2024-06-28 09:00:17 -0600

    configure gh actions role

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,8 +2,41 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-function run() {
-    core.notice('Hello from my custom Javascript Action!');
+async function run() {
+    try{
+        const bucket = core.getInput('bucket', {required: true});
+        const bucketRegion = core.getInput('bucket-region', {required: true});
+        const distFolder = core.getInput('dist-folder', {required: true});
+        const roleArn = core.getInput('role-arn', {required: true});
+    
+        await exec.exec(`aws sts assume-role --role-arn ${roleArn} --role-session-name deploy-s3-javascript-action`);
+    
+        let credentials = '';
+        exec.exec(`aws sts get-session-token`, [], {
+            listeners: {
+                stdout: (data) => {
+                    credentials += data.toString();
+                }
+            },
+        });
+    
+        const creds = JSON.parse(credentials);
+        const accessKeyId = creds.Credentials.AccessKeyId;
+        const secretAccessKey = creds.Credentials.SecretAccessKey;
+        const sessionToken = creds.Credentials.SessionToken;
+    
+        core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
+        core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
+        core.exportVariable('AWS_SESSION_TOKEN', sessionToken);
+    
+        const s3Uri = `s3://${bucket}`;
+        exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+    
+        core.notice('Hello from my custom Javascript Action!');
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
